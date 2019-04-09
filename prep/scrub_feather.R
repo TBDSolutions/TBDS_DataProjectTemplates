9e20676b0eb7a8a286189245f5ae9e0e87e7354d
emdwright <47824007+emdwright@users.noreply.github.com>
2019-04-09 11:25:57 -0400

    Create scrub_feather.R

---
+++ prep/scrub_feather.R
@@ -0,0 +1,26 @@
+## scrub.feather - file for removing PHI from all data sets and combining to make master.feather
+#General Order of Hierarchy
+#1. read
+#2. prep_
+#3. scrub
+#4. scrubd_prep
+#5. scrubd
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+########################################
+# Housekeeping!  
+########################################
+#rm(list = c())
