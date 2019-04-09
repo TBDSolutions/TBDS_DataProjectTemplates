26de0d9da9f0d37b7372cc2b6cc1b9b888e21481
emdwright <47824007+emdwright@users.noreply.github.com>
2019-04-09 11:25:54 -0400

    Create prep_.R

---
+++ prep/prep_.R
@@ -0,0 +1,28 @@
+## prep/transformation file 1 (first file after read files)
+#General Order of Hierarchy
+#1. read
+#2. prep_
+#3. scrub
+#4. scrubd_prep
+#5. scrubd
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+########################################
+# Housekeeping!  
+########################################
+#rm(list = c())
