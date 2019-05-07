c36060de63c74c6c0165b2e9538ee86a622a3eaf
emdwright <47824007+emdwright@users.noreply.github.com>
2019-05-07 11:43:26 -0400

    rename

--- read/read.R
+++ read/read.R
@@ -1,44 +1,48 @@
-#General Order of Hierarchy
-#1. read
-#2. prep_
-#3. scrub
-#4. scrubd_prep
-#5. scrubd
-
-
-
+# read.R
+#All copy to db related to data update goes here.  copy to db for prep files goes in the file the copy object was made
 library(DBI); library(odbc);library(dbplyr);library(tidyverse);library(lubridate);library(stringr)
 
-##################################### All Connections ############################################################
+####################################################################################################################################
+##App data updates ##  the major flaw iwth this plan is that we don't want raw data in db, we want clean data
+#####################
+#steps:
+#1.: Run the read.R file with the previous timepoint files
+#2.: Run update the file references in the test file and run it
+#3.: Compare row counts, column counts and any other important feature
+#4.: Update the read.R file with the new file references
+#5.: Run load.R to read in and load new data to the database
+#6.: Run all items in run_deploy.R
 
+##################################### All Connections ############################################################
 #### Make connections ####
-mdhhs_idd_db <- 
+
+conn <- 
   DBI::dbConnect(
     odbc::odbc(), 
     Driver = "SQL Server", 
     Server = Sys.getenv("tbd_server_address"), 
-    Database = "XXXX", 
+    Database = "conn", 
     UID = Sys.getenv("tbd_server_uid"), 
     PWD = Sys.getenv("tbd_server_pw"), 
     Port = 1433
   )
 
-## User name for file paths - also in the Global.R script
-source("prep/temp_scripts_final/filepath_fun.R")
 
-####################################################################################################################################
-## - flat files ##
-#####################
+##################################### SIS Online ############################################################
+
+##### Read in local text  #####
+#function so that file paths work no matter which developer is in the app
+source("prep/temp_scripts/filepath_fun.R")
+
+
+
+############################################# QI + Encounter ##########################################################
 
 ##### Read in local text  ####
 
-## Define local source and combine multiple files if needed
+##how long to run?
 
-dataset1 <- 
-  read_delim(
-    paste0(onedrive.path,""),  #update this file path monthly
-    delim = "\t", col_types = cols(.default = "c")
-  ) 
-#update variables and combine files if needed
-
-
+########################################
+# Housekeeping!  
+########################################
+#rm(list = c("",""))
